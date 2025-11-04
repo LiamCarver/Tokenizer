f8952c24ca977825efa55bceda16942627374c23
Liam <liamcarver002@gmail.com>
2025-11-04 18:20:20 +0200

    WIP - implement vocab update.

--- Tokenizer/Tokenizer.cs
+++ Tokenizer/Tokenizer.cs
@@ -1,34 +1,71 @@
-﻿using Newtonsoft.Json;
-
-namespace Tokenizer;
+﻿namespace Tokenizer;
 
 internal class Tokenizer
 {
     public void Tokenize(string input)
     {
-        var characterArray = input.ToCharArray().Select(character => character.ToString()).ToArray();
-        var characterPairFrequencies = GetAdjacentCharacterPairFrequencies(characterArray);
-        var sanityCheck = JsonConvert.SerializeObject(characterPairFrequencies, Formatting.Indented);
+        var originalVocabulary = input.ToCharArray().Select(character => character.ToString()).ToArray();
+        var characterPairFrequencies = GetAdjacentCharacterPairFrequencies(originalVocabulary);
+
+        var tokenWithMaximumCount = GetTokenWithMaximumCount(characterPairFrequencies);
+
+        var newVocabulary = GetUpdatedVocabulary(tokenWithMaximumCount, originalVocabulary);
     }
 
-    private Dictionary<Tuple<string, string>, int> GetAdjacentCharacterPairFrequencies(string[] characters)
+    private string[] GetUpdatedVocabulary(KeyValuePair<Tuple<string, string>, int> tokenWithMaximumCount, string[] currentVocabulary)
     {
-        Dictionary<Tuple<string, string>, int> adjacentCharacterFrequencies = new();
-        
-        for (var index = 0; index < characters.Length - 1; index++)
+        var firstTokenToMatch = tokenWithMaximumCount.Key.Item1;
+        var nextTokenToMatch = tokenWithMaximumCount.Key.Item2;
+
+        var newMergedToken = string.Concat(firstTokenToMatch, nextTokenToMatch);
+        var newVocabulary = new List<string>();
+
+        var index = 0;
+
+        while (index < currentVocabulary.Length - 2)
         {
-            var character = characters[index];
-            var nextCharacter = characters[index + 1];
+            var currentToken = currentVocabulary[index];
+            var nextToken = currentVocabulary[index + 1];
 
-            var characterPair = new Tuple<string, string>(character, nextCharacter);
-
-            if (adjacentCharacterFrequencies.TryGetValue(characterPair, out var frequency))
+            if (currentToken == firstTokenToMatch && nextToken == nextTokenToMatch)
             {
-                adjacentCharacterFrequencies[characterPair] = frequency + 1;
+                newVocabulary.Add(newMergedToken);
+                index += 2;
             }
             else
             {
-                adjacentCharacterFrequencies[characterPair] = 1;
+                newVocabulary.Add(currentToken);
+                index++;
+            }
+        }
+
+        return newVocabulary.ToArray();
+    }
+
+    private KeyValuePair<Tuple<string, string>, int> GetTokenWithMaximumCount(Dictionary<Tuple<string, string>, int> characterPairFrequencies)
+    {
+        var maximumFrequencyCount = characterPairFrequencies.Max(token => token.Value);
+        return characterPairFrequencies.First(pair => pair.Value == maximumFrequencyCount);
+    }
+
+    private Dictionary<Tuple<string, string>, int> GetAdjacentCharacterPairFrequencies(string[] tokens)
+    {
+        Dictionary<Tuple<string, string>, int> adjacentCharacterFrequencies = new();
+        
+        for (var index = 0; index < tokens.Length - 1; index++)
+        {
+            var currentToken = tokens[index];
+            var nextToken = tokens[index + 1];
+
+            var tokenPair = new Tuple<string, string>(currentToken, nextToken);
+
+            if (adjacentCharacterFrequencies.TryGetValue(tokenPair, out var frequency))
+            {
+                adjacentCharacterFrequencies[tokenPair] = frequency + 1;
+            }
+            else
+            {
+                adjacentCharacterFrequencies[tokenPair] = 1;
             }
         }
 
